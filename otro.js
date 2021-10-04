b844df62595774e3ba3e41bccb1d8a6f48751228
Eduin Santiago Arroyo <90221986+Earroyoj@users.noreply.github.com>
2021-10-04 09:57:40 -0500

    Delete otro.js

--- otro.js
+++
@@ -1,9 +0,0 @@
-import * as valida from "./validaciones.js";
-
-let usuario=document.forms['form_registro_usuario']['in_usuario'].value;
-let validacion=valida.validar_nombreUsuario(usuario);
-console.log(validacion);
-
-let contrasena=document.forms['form_registro_usuario']['in_contrasena'].value;
-let valida_contrasena=valida.validar_contrasena(contrasena);
-console.log(valida_contrasena);
